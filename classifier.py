a4c7c21232531242fe41a588ec0fca99e329f20f
Manolo Perez <31350932+Manolomon@users.noreply.github.com>
2020-04-29 21:23:37 -0500

    Update classifier.py

--- classifier.py
+++ classifier.py
@@ -47,21 +47,7 @@ documents = []
 from nltk.stem import WordNetLemmatizer
 
 stemmer = WordNetLemmatizer()
-
-for sen in range(0, len(X)):
-    # Remove all the special characters
-    document = re.sub(r'\W', ' ', str(X[sen]))
-    
-    # remove all single characters
-    document = re.sub(r'\s+[a-zA-Z]\s+', ' ', document)
-    
-    # Remove single characters from the start
-    document = re.sub(r'\^[a-zA-Z]\s+', ' ', document) 
-    
-    # Substituting multiple spaces with single space
-    document = re.sub(r'\s+', ' ', document, flags=re.I)
-    
-    # Removing prefixed 'b'
+hgfjhdgjfhd<gfj <gjk< fghkfhg< kfj g<kj<gf
     document = re.sub(r'^b\s+', '', document)
     
     # Converting to Lowercase
